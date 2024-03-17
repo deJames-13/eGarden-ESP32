97991ef2b26ebad8474e4af0e0d23fb56d9d41d1
deJames-13 <de.james013@gmail.com>
2024-03-17 17:15:16 +0800

    adsf

--- src/pin_config.h
+++ src/pin_config.h
@@ -3,11 +3,16 @@
 #define PIN_CONFIG_H
 
 // FIXME: Pin assignments
-#define DHT_PIN 5
-#define SOIL_MOISTURE_PIN 23
-#define WATER_LEVEL_PIN 4
-#define RELAY_PIN_FAN 18
-#define RELAY_PIN_VALVE 17
-#define BUZZER_PIN 19
+// SENSORS
+#define DHT_PIN 14           // GPIO14 D14
+#define SOIL_MOISTURE_PIN 32 // GPIO32 D32
+#define WATER_LEVEL_PIN 39   // GPIO39 VN
+
+// OUTPUTS
+#define OLED_SDA 21        // GPIO21 D21
+#define OLED_SCL 22        // GPIO22 D22
+#define RELAY_PIN_VALVE 18 // GPIO18 D18
+#define RELAY_PIN_FAN 5    // GPIO5 D5
+#define BUZZER_PIN 39
 
 #endif
